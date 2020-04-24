3e3d4323f614bee261e6cc3470fd789ebf5f2ccd
Direside <shane.gearon@gmail.com>
2020-04-23 18:49:42 -0700

    Initial React app.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './commit-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>zero</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
