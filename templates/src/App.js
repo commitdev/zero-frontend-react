afb224dc71177b88b0cf017eb6bd007c7bdccea0
Jason Dippel <dippelj12@gmail.com>
2020-08-04 15:04:53 -0700

    add router and basic home page with header

--- templates/src/App.js
+++ templates/src/App.js
@@ -1,61 +1,61 @@
-import React, { useEffect, useState } from "react";
-import logo from "./commit-logo.png";
-import "./App.css";
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
 
-import "./components/Info";
-import InfoPanel from "./components/Info";
+import './App.css'
+import Header from './components/Header'
 
-// Set config based on the environment variable the build was run under.
-let config = {};
-if (process.env.REACT_APP_CONFIG === "production") {
-  config = require("./config/production.json");
-} else if (process.env.REACT_APP_CONFIG === "staging") {
-  config = require("./config/staging.json");
-} else {
-  config = require("./config/development.json");
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+function PrivateRoute({ children, ...rest }) {
+  // TODO: perform actual check
+  const isAuthenticated = false
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  )
 }
 
 function App() {
-  const [data, setData] = useState({
-    info: {},
-    error: null,
-  });
-
-  const [status, setStatus] = useState({
-    code: "Checking...",
-  })
-
-  useEffect(() => {
-    fetch(`${config.backendURL}/status/about`)
-      .then(result => {
-        setStatus({
-          code: result.status,
-        })
-        return result.json()
-      })
-      .then(data => {
-        setData({
-          info: data,
-          error: null
-        })
-      })
-      .catch(error => {
-        setData({
-          info: {},
-          error: error
-        })
-      });
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>zero</h1>
-        <InfoPanel data={data} status={status} config={config} />
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ templates/src/pages/Home.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import logo from '../commit-logo.png'
+import './Home.css'
+
+import '../components/Info'
+import InfoPanel from '../components/Info'
+
+// Set config based on the environment variable the build was run under.
+let config = {}
+if (process.env.REACT_APP_CONFIG === 'production') {
+  config = require('../config/production.json')
+} else if (process.env.REACT_APP_CONFIG === 'staging') {
+  config = require('../config/staging.json')
+} else {
+  config = require('../config/development.json')
+}
+
+function Home() {
+  const [data, setData] = useState({
+    info: {},
+    error: null,
+  })
+
+  const [status, setStatus] = useState({
+    code: 'Checking...',
+  })
+
+  useEffect(() => {
+    fetch(`${config.backendURL}/status/about`)
+      .then((result) => {
+        setStatus({
+          code: result.status,
+        })
+        return result.json()
+      })
+      .then((data) => {
+        setData({
+          info: data,
+          error: null,
+        })
+      })
+      .catch((error) => {
+        setData({
+          info: {},
+          error: error,
+        })
+      })
+  }, [])
+
+  return (
+    <main className="Home">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>zero</h1>
+      <InfoPanel data={data} status={status} config={config} />
+    </main>
+  )
+}
+
+export default Home
