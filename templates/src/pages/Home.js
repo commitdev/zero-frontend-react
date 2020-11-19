d0e4cb7c9a3e3c1cdc7823ec3bdec2b986edb54b
David Cheung <davidcheung@live.ca>
2020-11-18 19:27:27 -0500

    example for private/normal data (#29)
    * example for private/normal data
    
    * fixup! example for private/normal data
    
    * remove /api prefix in backend paths

--- templates/src/pages/Home.js
+++ templates/src/pages/Home.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import './Home.css'
 import config from '../config';
-
-import '../components/Info'
+import './Home.css'
 import InfoPanel from '../components/Info'
+<%if eq (index .Params `userAuth`) "yes" %>import Card from '../components/Card'<% end %>
 
 function Home() {
   const [data, setData] = useState({
@@ -14,32 +13,51 @@ function Home() {
   const [status, setStatus] = useState({
     code: 'Checking...',
   })
+<%if eq (index .Params `userAuth`) "yes" %>
+  const [privateState, setPrivateData] = useState({
+    isLoading: true,
+    data: {},
+  });
+
+  const fetchPrivateData = async(data) =>  {
+    const uri = `${config.backendURL}/auth/userInfo`
+    const resp = await fetch(uri,{credentials : "include"});
+    return {
+      data: await resp.json(),
+    }
+  }<% end %>
+
+  const fetchInfoPanel = async() => {
+    const resp = await fetch(`${config.backendURL}/status/about`, { credentials: "include" });
+    const code = resp.status
+    const info = await resp.json()
+    return { code, info };
+  };
+
 
 useEffect(() => {
-    fetch(`${config.backendURL}/status/about`, { credentials: "include" })
-      .then((result) => {
-        setStatus({
-          code: result.status,
-        })
-        return result.json()
-      })
-      .then((data) => {
-        setData({
-          info: data,
-          error: null,
-        })
-      })
-      .catch((error) => {
-        setData({
-          info: {},
-          error: error,
-        })
-      })
-  }, [])
+  fetchInfoPanel().then(({code, info}) => {
+      setStatus({ code });
+      setData({ info, error: null })
+    })
+    .catch((error) => {
+      setData({ info: {}, error: error })
+    });
+<%if eq (index .Params `userAuth`) "yes" %>
+    fetchPrivateData().then(setPrivateData);<% end %>
+}, [])
 
   return (
     <main className="Home">
       <InfoPanel data={data} status={status} config={config} />
+<%if eq (index .Params `userAuth`) "yes" %>
+      <Card header="Authenticated Data">
+        {
+          privateState.isLoading ?
+            <div>Fetching data...</div> :
+            <pre>{ JSON.stringify(privateState.data, null , 2) }</pre>
+        }
+      </Card><% end %>
     </main>
   )
 }
